08a4c01301cc9c5428535a3441d0489ada3b07a5
Aksharasanker <159803324+Aksharasanker@users.noreply.github.com>
2024-04-10 19:36:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
@@ -6,13 +5,18 @@ import './App.css'
 import { useState } from 'react';
 
 
+
 function App() {
+  const [age, setAge]=useState(null);
     const [height, setHeight]=useState(null);
     const [weight, setWeight]=useState(null);
     const [bmi, setBMI]=useState(null);
     const [cond, setCond]=useState("");
 
-
+    const onSetAge = e =>{
+      const value=e.target.value;
+      setAge(value);
+    }
 
     const onSetHeight = e =>{
       const value=e.target.value;
@@ -43,21 +47,33 @@ function App() {
        setCond(bmiCond)
        }
     
+
+       const handleReset=()=>{
+        setAge(null)
+        setHeight(null)
+        setWeight(null)
+        setBMI(null)
+        setAge(0)
+        setHeight(0)
+        setWeight(0)
+        setBMI(0)
+                
+      }
   return (
     <>
       <div className='main'>
         <div className='sub'>
         <h1>BMI CALCULATOR</h1>
            <form>
-              <TextField id="outlined-basic" color='success'name='Age' placeholder='Age' variant="outlined" className='w-100 mt-3'   onChange={(e)=>validate(e)}/> 
+              <TextField id="outlined-basic" color='success'name='Age' placeholder='Age' variant="outlined" className='w-100 mt-3' value={age}  onChange={onSetAge}/> 
               <TextField id="outlined-basic" color='success' name='Height'placeholder='Height in Meter' variant="outlined" className='w-100 mt-3'value={height} onChange={onSetHeight}/>
               <TextField id="outlined-basic" color='success' name='Weight' placeholder='Weight in KG' variant="outlined" className='w-100 mt-3' value={weight} onChange={onSetWeight}/>
             
             <label><input type='radio' name="Gender" value="Male" className='m-2 mt-3'></input>Male</label>
             <label><input type='radio' name="Gender"  value="Female" className='m-2 mt-3'></input>Female</label><br></br>
 
-              <Button  variant="outlined" className='w-25 mt-3 m-1' color='success'>Reset</Button>
-              <Button disabled={!weight || !height } variant="contained"className='w-25 mt-3 m-1' color='success'onClick={calculateBMI} >Submit</Button>
+              <Button  onClick={handleReset} variant="outlined" className='w-25 mt-3 m-1' color='success'>Reset</Button>
+              <Button disabled={!weight || !height ||!age } variant="contained"className='w-25 mt-3 m-1' color='success'onClick={calculateBMI} >Submit</Button>
             
 
               <div className='w-100 d-flex justify-content-center align-items-center result rounded mt-5 shadow flex-column' id="msg">
